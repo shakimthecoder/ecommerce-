ea182ec6e2db9a84d7baf8d2f9ff16a8f7549158
amenraprod <amenraprod@gmail.com>
2019-09-02 21:49:22 -0400

    added sneakers

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -21,4 +21,14 @@ const Homepage = () => (
       </div>
      </div>
    </div>
+   <div className="shop-container">
+   <div className="menu-directory">
+    <div className="menu-item">
+     <div className="content"> 
+      <h1 className="title">SNEAKERS</h1>
+       <span className="subtitle">SHOP NOW</span>
+      </div>
+     </div>
+    </div>
+  </div>
 );
