4f65a36f99ebb59cea665570b57070ad607a0d88
amenraprod <amenraprod@gmail.com>
2019-09-02 21:45:50 -0400

    structured homepage component

---
+++ src/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Homepage = () => (
+    <div className="shop-container">
+     <div className="menu-directory">
+      <div className="menu-item">
+       <div className="content"> 
+        <h1></h1>
+        </div>
+       </div>
+      </div>
+    </div>
+);
